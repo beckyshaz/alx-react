73b9b284b916326fd4e864501b66855e29e1602a
beckyshaz <sharonsakah8@gmail.com>
2024-10-29 02:56:03 +0000

    css and images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production'
+  module: {
+    rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader']},
+      { 
+      test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      type: 'asset/resource',
+    },
+    ]
+  },
+  
+};
+
